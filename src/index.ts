9df59aaa971b6d6fc7f8cb9d65ea3736a884d835
Sellucas <lucassellmachado@gmail.com>
2023-08-04 15:52:13 -0300

    feat: mongoDB setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 const app = express()
 
@@ -21,3 +22,8 @@ server.listen(8080, () => {
   console.log("Server running on http://localhost:8080/")
 })
 
+const MONG_URL = 'mongodb+srv://admin:M7yew5rCNgMGxc3T@api-rest.rellvsj.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.Promise = Promise
+mongoose.connect(MONG_URL)
+mongoose.connection.on('error', (error: Error) => console.log(error))
