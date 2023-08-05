32c9f3dbb6f8c9fbe1f9d120b53e0910ff7426ba
Sellucas <lucassellmachado@gmail.com>
2023-08-04 21:42:46 -0300

    feat: auth rout and login

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import compression from 'compression'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
+import router from './router'
+
 const app = express()
 
 app.use(cors({
@@ -22,8 +24,10 @@ server.listen(8080, () => {
   console.log("Server running on http://localhost:8080/")
 })
 
-const MONG_URL = 'mongodb+srv://admin:M7yew5rCNgMGxc3T@api-rest.rellvsj.mongodb.net/?retryWrites=true&w=majority'
+const MONGO_URL = 'mongodb+srv://admin:M7yew5rCNgMGxc3T@api-rest.rellvsj.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose.Promise = Promise
-mongoose.connect(MONG_URL)
+mongoose.connect(MONGO_URL)
 mongoose.connection.on('error', (error: Error) => console.log(error))
+
+app.use('/', router())
