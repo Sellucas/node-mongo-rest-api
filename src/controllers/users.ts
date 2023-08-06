f369e9849b18d75cfa0ac252df585623e4dbb61a
Sellucas <lucassellmachado@gmail.com>
2023-08-06 15:09:40 -0300

    feat: delete user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,12 +1,26 @@
 import express from 'express'
 
-import { getUsers } from '../db/users'
+import { deleteUserById, getUsers } from '../db/users'
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
   try {
     const users = await getUsers()
 
-    return res.status(400).json(users)
+    return res.status(200).json(users)
+
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(400)
+  }
+};
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await deleteUserById(id)
+
+    return res.json(deleteUser)
 
   } catch (error) {
     console.log(error)
