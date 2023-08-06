a7dfcf42e93466237e887646a98a1b4bd040578a
Sellucas <lucassellmachado@gmail.com>
2023-08-06 15:38:54 -0300

    feat: update user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { deleteUserById, getUsers } from '../db/users'
+import { deleteUserById, getUserById, getUsers } from '../db/users'
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
   try {
@@ -26,4 +26,26 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
     console.log(error)
     res.sendStatus(400)
   }
-}
+};
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+  try {
+    const { id } = req.params
+    const { userName } = req.body
+
+    if (!userName) {
+      return res.sendStatus(400)
+    }
+
+    const user = await getUserById(id)
+
+    user.userName = userName
+    await user.save()
+
+    return res.status(200).json(user).end()
+
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(400)
+  }
+};
